707e43e7565f37915b443ea5a7c64d99957a8d87
Trevor Payne <crashandexplode@hotmail.com>
2018-06-19 14:06:10 -0700

    Added PyQt exception handling

--- QWizard_Example/QWizard_Example.py
+++ QWizard_Example/QWizard_Example.py
@@ -1,16 +1,34 @@
 import sys
-from Qt import QtWidgets
+import Qt
+from Qt import QtWidgets, QtGui
+
+print ('Qt Version: ' + Qt.__qt_version__)
+print ('Binding Version: ' + Qt.__binding_version__)
+print ('Is this PySide2? ', Qt.IsPySide2)
+print ('Is this PyQt5? ', Qt.IsPyQt5)
+print ('Is this PySide? ', Qt.IsPySide)
+print ('Is this PyQt4? ', Qt.IsPyQt4)
 
 # CREATE WIZARD, WATERMARK, LOGO, BANNER
 app = QtWidgets.QApplication(sys.argv)
 wizard = QtWidgets.QWizard()
 wizard.setWizardStyle(QtWidgets.QWizard.ModernStyle)
-wizard.setPixmap(QtWidgets.QWizard.WatermarkPixmap,
-                'Watermark.png')
-wizard.setPixmap(QtWidgets.QWizard.LogoPixmap,
-                'Logo.png')
-wizard.setPixmap(QtWidgets.QWizard.BannerPixmap,
-                'Banner.png')
+
+try: # PYSIDE
+    wizard.setPixmap(QtWidgets.QWizard.WatermarkPixmap,
+                    'Watermark.png')
+    wizard.setPixmap(QtWidgets.QWizard.LogoPixmap,
+                    'Logo.png')
+    wizard.setPixmap(QtWidgets.QWizard.BannerPixmap,
+                    'Banner.png')
+except TypeError: # PYQT5
+    wizard.setPixmap(QtWidgets.QWizard.WatermarkPixmap,
+            QtGui.QPixmap('Watermark.png'))
+    wizard.setPixmap(QtWidgets.QWizard.LogoPixmap,
+            QtGui.QPixmap('Logo.png'))
+    wizard.setPixmap(QtWidgets.QWizard.BannerPixmap,
+            QtGui.QPixmap('Banner.png'))
+
 
 # CREATE PAGE 1, LINE EDIT, TITLES
 page1 = QtWidgets.QWizardPage()
@@ -19,10 +37,18 @@ page1.setSubTitle('1111111111')
 lineEdit = QtWidgets.QLineEdit()
 hLayout1 = QtWidgets.QHBoxLayout(page1)
 hLayout1.addWidget(lineEdit)
-page1.registerField('myField*', 
-    lineEdit,
-    lineEdit.text(),
-    'textChanged')
+
+try: # PYSIDE
+    page1.registerField('myField*', 
+        lineEdit,
+        lineEdit.text(),
+        'textChanged')
+except TypeError: # PYQT
+    page1.registerField('myField*', 
+        lineEdit,
+        lineEdit.text(),
+        lineEdit.textChanged)
+
 
 # CREATE PAGE 2, LABEL, TITLES
 page2 = QtWidgets.QWizardPage()
